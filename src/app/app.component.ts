7911583244dd84ae627ffbf253e05dfa0abe7f58
Hansel <hansel809@hotmail.com>
2017-09-22 10:45:54 -0400

    Tested electron dialogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,28 @@ export class AppComponent {
         this.electronService.os.arch());
     }
   }
+
+  openFile() {
+    if (this.electronService.isElectron()) {
+      this.electronService.remote.dialog.showOpenDialog({
+        title: 'find file for me',
+        properties: ['openFile', 'showHiddenFiles']
+      }, (filePaths: string[]) => {
+        console.log(filePaths);
+      });
+    }
+  }
+
+  showMessage() {
+    if (this.electronService.isElectron()) {
+      this.electronService.remote.dialog.showMessageBox({
+        title: 'test',
+        message: 'test_message',
+        type: 'info',
+        buttons: ['OK', 'Cancel']
+      }, (response: number, checkboxChecked: boolean) => {
+        console.log(response, checkboxChecked);
+      });
+    }
+  }
 }
--- src/app/services/electron.service.ts
+++ src/app/services/electron.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, remote } from 'electron';
 import * as os from 'os';
 
 declare global {
@@ -16,11 +16,13 @@ declare global {
 export class ElectronService {
 
   ipcRenderer: typeof ipcRenderer;
+  remote: typeof remote;
   os: typeof os;
 
   constructor() {
     if (this.isElectron()) {
       this.ipcRenderer = window.electron.ipcRenderer;
+      this.remote = window.electron.remote;
       this.os = window.os;
     }
   }
