838835e3e7a7f0198d02a5203d81a6df057e160e
Hansel <hansel809@hotmail.com>
2017-09-21 22:26:52 -0400

    Added electron live-reloading

--- src/desktop/main.ts
+++ src/desktop/main.ts
@@ -2,6 +2,10 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
 
+require('electron-reload')(__dirname, {
+  electron: require('electron-prebuilt')
+})
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
