d38eab47fcabf1e07f6f0f3811d1eacd8e2913db
Hansel <hansel809@hotmail.com>
2017-10-16 08:10:11 -0400

    Fixed jquery not found when running the app with Electron

--- native.js
+++ native.js
@@ -1,7 +1,11 @@
 if (window && window.process && window.process.type) {
-    window._electron = require('electron');
-    window._si = require('systeminformation');
-    window._crypto = require('crypto');
-    window._fs = require('fs');
-    window._os = require('os');
+  window.$ = window.jQuery = require('jquery');
+  window.Popper = require('popper.js');
+  require('bootstrap');
+
+  window._electron = require('electron');
+  window._si = require('systeminformation');
+  window._crypto = require('crypto');
+  window._fs = require('fs');
+  window._os = require('os');
 }
